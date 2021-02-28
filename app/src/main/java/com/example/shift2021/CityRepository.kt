aabda025ccfe027121d267f33308ec4e37e94fd3
ArtemGomer <happycrown30062000@gmail.com>
2021-02-28 16:19:30 +0700

    add cityWeather and repository classes

---
+++ app/src/main/java/com/example/shift2021/CityRepository.kt
@@ -0,0 +1,18 @@
+package com.example.shift2021
+
+import android.app.Application
+
+class CityRepository {
+    private val cities = mutableListOf(
+            CityWeather(0, "Moscow", "Russia", "-10", "sunny"),
+            CityWeather(0, "Berlin", "Germany", "0", "cloudy"),
+            CityWeather(0, "Paris", "France", "-3", "snowy"),
+            CityWeather(0, "London", "United Kingdom", "+4", "rainy"),
+            CityWeather(0, "Kiev", "Ukraine", "-15", "snowy"),
+            CityWeather(0, "Minsk", "Belarus", "-7", "sunny")
+    )
+
+    fun getCityById(id: Long): CityWeather? = cities.firstOrNull { it.id == id }
+
+    fun getCities(): List<CityWeather> = cities
+}
---
+++ app/src/main/java/com/example/shift2021/CityWeather.kt
@@ -0,0 +1,9 @@
+package com.example.shift2021
+
+data class CityWeather (
+        val id: Long,
+        val name: String,
+        val country: String,
+        val temperature: String,
+        val precipitation: String
+        )
