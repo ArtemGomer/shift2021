7963f69774f6a49f9af527dd9b57fa8c9886fc13
ArtemGomer <happycrown30062000@gmail.com>
2021-02-28 18:28:12 +0700

    ugly, but working

--- app/src/main/java/com/example/shift2021/DetailsActivity.kt
+++ app/src/main/java/com/example/shift2021/DetailsActivity.kt
@@ -1,11 +1,63 @@
 package com.example.shift2021
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.shift2021.cityStuff.CityRepository
+import com.example.shift2021.cityStuff.CityWeatherApplication
 
 class DetailsActivity : AppCompatActivity() {
+
+    companion object {
+
+        private const val EXTRA_ID = "EXTRA_ID"
+
+        fun start(context: Context, id: Long) {
+            val intent = Intent(context, DetailsActivity::class.java)
+            intent.putExtra(EXTRA_ID, id)
+            context.startActivity(intent)
+        }
+    }
+
+    private lateinit var cityRepository: CityRepository
+
+    private lateinit var nameText: TextView
+    private lateinit var temperatureText: TextView
+    private lateinit var countryText: TextView
+    private lateinit var precipitationText: TextView
+    private lateinit var backButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        cityRepository = (application as CityWeatherApplication).cityRepository
         setContentView(R.layout.activity_details)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val id = intent.getLongExtra(EXTRA_ID, 0)
+        val city = cityRepository.getCityById(id)
+
+        if (city != null) {
+            nameText = findViewById(R.id.city_name_text)
+            countryText = findViewById(R.id.city_country_text)
+            temperatureText = findViewById(R.id.city_temperature_text)
+            precipitationText = findViewById(R.id.city_precipitation_text)
+            backButton = findViewById(R.id.back_button)
+
+            nameText.text = city.name
+            temperatureText.text = getString(R.string.temperature_format, city.temperature)
+            countryText.text = city.country
+            precipitationText.text = getString(R.string.precipitation_format, city.precipitation)
+            backButton.setOnClickListener {
+                finish()
+            }
+        } else {
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/shift2021/cityAdapter/CityAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.shift2021.cityAdapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shift2021.R
+import com.example.shift2021.cityStuff.CityWeather
+
+class CityAdapter(private val onClick: (CityWeather) -> Unit) : RecyclerView.Adapter<CityHolder>() {
+
+    var cities: List<CityWeather> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        return CityHolder(view, onClick)
+    }
+
+    override fun onBindViewHolder(holder: CityHolder, position: Int) {
+        val person = cities[position]
+        holder.bind(person)
+    }
+
+    override fun getItemCount(): Int = cities.count()
+}
+
+class CityHolder(itemView: View, private val onClick: (CityWeather) -> Unit) : RecyclerView.ViewHolder(itemView) {
+
+    private val cityText: TextView = itemView.findViewById(R.id.cityText)
+    private val temperatureText: TextView = itemView.findViewById(R.id.temperatureText)
+
+    fun bind(city: CityWeather) {
+        cityText.text = city.name
+        temperatureText.text =itemView.context.getString(R.string.temperature_format, city.temperature)
+        itemView.setOnClickListener { onClick(city) }
+    }
+}
