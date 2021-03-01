8fff54686412d0e915eaebd0b7db753843a21815
ArtemGomer <happycrown30062000@gmail.com>
2021-03-01 20:29:40 +0700

    add pictures. Fix bug in repo

--- app/src/main/java/com/example/shift2021/cityStuff/CityRepository.kt
+++ app/src/main/java/com/example/shift2021/cityStuff/CityRepository.kt
@@ -3,11 +3,11 @@ package com.example.shift2021.cityStuff
 class CityRepository {
     private val cities = mutableListOf(
             CityWeather(0, "Moscow", "Russia", "-10", "sunny"),
-            CityWeather(0, "Berlin", "Germany", "0", "cloudy"),
-            CityWeather(0, "Paris", "France", "-3", "snowy"),
-            CityWeather(0, "London", "United Kingdom", "+4", "rainy"),
-            CityWeather(0, "Kiev", "Ukraine", "-15", "snowy"),
-            CityWeather(0, "Minsk", "Belarus", "-7", "sunny")
+            CityWeather(1, "Berlin", "Germany", "0", "cloudy"),
+            CityWeather(2, "Paris", "France", "-3", "snowy"),
+            CityWeather(3, "London", "United Kingdom", "+4", "rainy"),
+            CityWeather(4, "Kiev", "Ukraine", "-15", "snowy"),
+            CityWeather(5, "Minsk", "Belarus", "-7", "sunny")
     )
 
     fun getCityById(id: Long): CityWeather? = cities.firstOrNull { it.id == id }
